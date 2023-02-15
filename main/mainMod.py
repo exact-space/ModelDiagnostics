403aabf15b67a66c8e32efde74dbb8809423a29a
ibnerasheed <ibnerasheed@gmail.com>
2023-02-15 16:53:08 +0530

    fetching and setting basic flow

---
+++ main/mainMod.py
@@ -0,0 +1,171 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+import pandas as pd
+import numpy as np
+import json
+import requests
+
+import os
+import datetime
+import time
+import statistics
+import math
+import sys
+
+# import timeseries as ts
+import os
+import datetime
+import time
+import statistics
+import math
+import sys
+import json
+import os
+import platform
+import re
+import time as t
+from datetime import date, datetime, time, timedelta
+
+import platform
+
+version = platform.python_version().split(".")[0]
+if version == "3":
+    import app_config.app_config as cfg
+elif version == "2":
+    import app_config as cfg
+config = cfg.getconfig()
+
+
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Dropout
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import make_scorer
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pickle
+from sklearn.preprocessing import MinMaxScaler
+import logging as lg
+
+
+def getModelFromId(modelId):
+    query = {"id": modelId}
+    urlQuery = (
+        config["api"]["meta"]
+        + '/optimizationmodels?filter={"where":'
+        + json.dumps(query)
+        + "}"
+    )
+    response = requests.get(urlQuery)
+    if response.status_code == 200:
+        # print(response.status_code)
+        print("Got model details successfully.....")
+        modelDetails = json.loads(response.content)
+    else:
+        modelDetails = {}
+        print("Did not get model details successfully.....")
+        print(response.status_code)
+        print(response.content)
+    return modelDetails
+
+
+def getTagsFromModelDetailes(modelDetails):
+    inputTags = []
+    outputTags = []
+
+    for ip in modelDetails[0]["inputs"]:
+        inputTags.append(ip["dataTagId"])
+    for op in modelDetails[0]["outputs"]:
+        outputTags.append(op["dataTagId"])
+
+    modelDict = {
+        "inputTags": inputTags,
+        "outputTags": outputTags,
+        "id": modelDetails[0]["id"],
+    }
+
+    return modelDict
+
+
+def getLatestVersionHistoryFromModelId(modelId):
+    urlQuery = (
+        config["api"]["meta"] + "/optimizationmodels/" + modelId + "/modelversions"
+    )
+
+    response = requests.get(urlQuery)
+
+    if response.status_code == 200:
+        versionHistory = json.loads(response.content)
+        versionHistory = versionHistory[-1]
+        currentVersion = versionHistory["version"]
+    else:
+        print("Getting latest version history failed....")
+        versionHistory = {}
+        currentVersion = 0
+        print(response.status_code)
+        print(response.content)
+    return versionHistory, currentVersion
+
+
+def downloadingFile(fileName):
+    url = config["api"]["meta"] + "/attachments/models/download/" + fileName
+    res = requests.get(url)
+    open(fileName, "wb").write(res.content)
+    print("Downloading completed for file " + str(fileName))
+
+
+def gettingTimeStamp(date):
+    date = datetime.datetime.strptime(date, "%d-%m-%YT%H:%M:%S")
+    timestamp = datetime.datetime.timestamp(date)
+    return timestamp
+
+
+modelId = "63a0b29d8c8c0600070c22e0"
+
+modelDetails = getModelFromId(modelId)
+
+# getting model details:
+modelTags = getTagsFromModelDetailes(modelDetails)
+
+inputTags = modelTags["inputTags"]
+outputTags = modelTags["outputTags"]
+
+versionHist, currentVersion = getLatestVersionHistoryFromModelId(modelId)
+
+h5FileName = modelId + "_Version" + str(currentVersion) + "_ANN_Regression.h5"
+transFileName = modelId + "_Version" + str(currentVersion) + "_Transformer.pkl"
+
+# downloadingFile(h5FileName)
+# downloadingFile(transFileName)
+
+
+refStartTime = versionHist["modelTime"]["train"]
+refStartTime = refStartTime[0]["startTime"]
+refStartTime = gettingTimeStamp(refStartTime)
+refEndTime = versionHist["modelTime"]["train"]
+refEndTime = refEndTime[0]["endTime"]
+refEndTime = gettingTimeStamp(refEndTime)
+
+today = datetime.today()
+v = datetime.combine(today, time.min)
+l = v - timedelta(days=3)
+currentStartTime = (
+    int(t.mktime(l.timetuple())) * 1000 - int(5.5 * 60 * 60 * 1000) - 1000
+)
+currenEndTime = int(t.mktime(v.timetuple())) * 1000 - int(5.5 * 60 * 60 * 1000) - 1000
+
+print(currentStartTime)
+print(currenEndTime)
